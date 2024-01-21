dad86c21a52c3c4296e96cea55811f9ef409555f
jbcharrier <jbcharrier33@gmail.com>
2024-01-21 21:53:39 +0100

    Structure OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './components/main/main.component';
+import { QuiSuisJeComponent } from './components/main/qui-suis-je/qui-suis-je.component';
+import { ConsultationComponent } from './components/main/consultation/consultation.component';
+import { ContactComponent } from './components/main/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    QuiSuisJeComponent,
+    ConsultationComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/main/qui-suis-je/qui-suis-je.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qui-suis-je',
+  templateUrl: './qui-suis-je.component.html',
+  styleUrls: ['./qui-suis-je.component.sass']
+})
+export class QuiSuisJeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
